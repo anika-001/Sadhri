0620840607058bed98692790293d9d04175d39bc
unknown <jhanvisdesai@gmail.com>
2021-08-20 01:45:13 +0530

    add final changes

--- src/app/JSON data/Navlinks.ts
+++ src/app/JSON data/Navlinks.ts
@@ -12,16 +12,13 @@ export let navbar = [
     {
         // "role":"user",
         "name":"Home",
-<<<<<<< HEAD
-        "link":"home",
-=======
+        // "link":"home",
         "link":"/home",
     },
     {
         // "role":"user",
         "name":"Clock",
         "link":"/digitalclock",
->>>>>>> fa956ea340269d286fe32a9f11909244f740624b
     }
     // {
     //     // "role":"user",
--- src/app/notesandvid/notesandvid.component.ts
+++ src/app/notesandvid/notesandvid.component.ts
@@ -6,8 +6,7 @@ import { AngularFirestore } from '@angular/fire/firestore';
 import { Router } from '@angular/router';
 import { FormControl, FormGroup, ValidationErrors, Validators } from '@angular/forms';
 
-interface IVidLinks 
-{
+interface IVidLinks {
   link: SafeUrl;
 }
 
@@ -22,9 +21,12 @@ export class NotesandvidComponent implements OnInit {
   i: any = 0;
   splitted: any;
   url: any;
-  userID:any;
-  videos:any;
-  video:any = [];
+  userID: any;
+  videos: any;
+  video: any = [];
+  bruh = 0;
+  notes: any;
+  // docid:any;
 
   formnote = new FormGroup({
     note: new FormControl('', [Validators.required]),
@@ -34,8 +36,7 @@ export class NotesandvidComponent implements OnInit {
     link: new FormControl('', [Validators.required]),
     // date: new FormControl('', [Validators.required])
   })
-  btnClick()
-  {
+  btnClick() {
     this.splitted = (this.formvids.value.link).split("v=");
     this.makelink(this.splitted[1]);
   }
@@ -46,16 +47,7 @@ export class NotesandvidComponent implements OnInit {
     content: new FormControl(' ')
   })
 
-  savenote()
-  {
-    console.log(this.formnote.value);
-    const dbpath = `Notes/${this.userID}/notes`;
-    this.db.doc(dbpath).update({ Notes: this.formnote.value, uid: this.userID }).then(e => {
-      // 
-      console.log(e)
-    }).
-      catch(e => { console.log(e) });
-  }
+
 
   ngOnInit(): void {
     this.as.getUserState()
@@ -64,16 +56,55 @@ export class NotesandvidComponent implements OnInit {
         else {
           this.userID = user.uid;
           // this.getShortcuts();
-      this.getvids()
+          this.getvids()
+          this.getnotes()
 
         }
         console.log(this.userID)
         // this.getProjects();
       })
-  }
 
-  savevidlink()
-  {
+  }
+  getnotes() {
+    this.db.collection("Notes").doc(this.userID).collection("notes").snapshotChanges().subscribe(response => {
+      this.notes = response;
+      // console.log(response);
+      console.log(this.notes[0].payload.doc.data().Notes);
+      this.notes = this.notes[0].payload.doc.data().Notes;
+      this.formnote.get("note")?.setValue(this.notes);
+    })
+  }
+  savenote() {
+    this.bruh = 0;
+    console.log(this.formnote.value);
+    const dbpath = `Notes/${this.userID}/notes`;
+    this.db.collection("Notes").doc(this.userID).collection("notes").snapshotChanges().subscribe(res => {
+      if (this.bruh > 0) return;
+      console.log(res.length)
+      if (res.length == 0) {
+        this.db.collection(dbpath).add({ Notes: this.formnote.value.note, uid: this.userID }).then(e => {
+          console.log(e)
+        }).
+          catch(e => { console.log(e) });
+        this.bruh = 1;
+        console.log("its null");
+      }
+      else {
+        let docid = res[0].payload.doc.id;
+        console.log(docid);
+        this.db.collection("Notes").doc(this.userID).collection("notes").doc(docid).update({ Notes: this.formnote.value.note }).then(e => {
+          console.log(e)
+          this.bruh = 1;
+        }).
+          catch(e => { console.log(e) });
+      }
+
+    })
+    // .get().snapshotChanges()
+
+
+  }
+  savevidlink() {
     const dbpath = `Embedvids/${this.userID}/vids`;
     this.db.collection(dbpath).add({ Links: this.url, uid: this.userID }).then(e => {
       // 
@@ -83,12 +114,12 @@ export class NotesandvidComponent implements OnInit {
   }
 
   makelink(Vidlink: string) {
-    this.url = "https://www.youtube.com/embed/" + Vidlink ;
+    this.url = "https://www.youtube.com/embed/" + Vidlink;
     // this.vidlinks.push({ link: (this._sanitizer.bypassSecurityTrustResourceUrl(this.url)) })
     this.savevidlink();
     // console.log(this.vidlinks);
   }
-  getvids(){
+  getvids() {
     this.db.collection("Embedvids").doc(this.userID).collection("vids").snapshotChanges().subscribe(response => {
       this.videos = response;
       // console.log(response);
@@ -96,8 +127,7 @@ export class NotesandvidComponent implements OnInit {
 
       // this.i = 0;
 
-      for(let val of this.videos)
-      {
+      for (let val of this.videos) {
         this.video = [];
         console.log(val.payload.doc.data()["Links"])
         this.video.push({ link: (this._sanitizer.bypassSecurityTrustResourceUrl(val.payload.doc.data()["Links"])) })
@@ -107,7 +137,7 @@ export class NotesandvidComponent implements OnInit {
 
       // console.log(this.videos[0].payload.doc.data()["Links"])
 
-  })
-}
+    })
+  }
 
 }
